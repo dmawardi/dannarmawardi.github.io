8f08bf79731b2d76f592d99ad1568751411d331d
Dannar Mawardi <dmawardi@gmail.com>
2019-08-16 11:29:26 +1000

    Hover CSS effect added

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,3 +6,14 @@ $('.card-body').on('click', function() {
     console.log('clicking a button on nav!:');
     console.log(this);
 })
+
+$('.card-body').hover(function(){
+    // Mouse Enter
+    $('#hovered').show();
+    $('#notHovered').hide();
+
+}, function(){
+    // Mouse Leave
+    $('#hovered').hide();
+    $('#notHovered').show();
+})
