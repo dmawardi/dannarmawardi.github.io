f292d5ba146541b627ddd598f6cbba059dd5659f
Dannar Mawardi <dmawardi@gmail.com>
2019-08-16 15:48:56 +1000

    Completed first page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,3 @@
-console.log('connected!');
 
 $('.card-body').on('click', function () {
     console.log('clicking a button on nav!:');
@@ -7,7 +6,6 @@ $('.card-body').on('click', function () {
 
 $('.card-body').hover(function () {
     // Mouse Enter
-    console.log(this);
     // Grab ID of hovered element
     var cardID = $(this).attr('id')
 
@@ -16,11 +14,26 @@ $('.card-body').hover(function () {
     $('#' + cardID + ' #notHovered').hide();
 
 }, function () {
+    // Mouse Leave
 
     // Grab ID of hovered element
-    var cardID = $(this).attr('id')
-    
-    // Mouse Leave
+    var cardID = $(this).attr('id');
+
+    // Use card id to select icons within and show/hide
     $('#' + cardID + ' #hovered').hide();
     $('#' + cardID + ' #notHovered').show();
 })
+
+$(document).ready(function(){
+    console.log('connected! Document ready!');
+
+    // Get array of objects that need to be initially hidden
+    let toBeHiddenObjects = $('.initiallyHidden');
+
+    // For each object in list of objects to be hidden
+    for (let i = 0; i < toBeHiddenObjects.length; i++) {
+        // Convert to JQuery object then hide
+        $(toBeHiddenObjects[i]).hide();
+    }
+
+});
